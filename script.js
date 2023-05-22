5fe7fc07f00f4b1876f902f78ee9013f40a206db
Mark Truong <tmarky002@gmail.com>
2023-05-21 21:33:59 -0700

    added login and registration, added session storage for user input

--- script.js
+++ script.js
@@ -1,53 +1,109 @@
-// Get the form and events list elements
-const form = document.getElementById('eventForm');
-const eventsList = document.getElementById('eventsList');
+// Event List
+const eventList = document.getElementById('eventList');
+let eventsData = JSON.parse(localStorage.getItem('eventsData')) || [];
 
-// Event handler for form submission
-form.addEventListener('submit', function (event) {
-  event.preventDefault(); // Prevent form submission
+// Populate event list
+function populateEventList() {
+  const tbody = eventList.getElementsByTagName('tbody')[0];
+  tbody.innerHTML = '';
 
-  // Get form values
-  const title = document.getElementById('title').value;
-  const venue = document.getElementById('venue').value;
-  const date = document.getElementById('date').value;
-  const time = document.getElementById('time').value;
+  eventsData.forEach((event, index) => {
+    const row = document.createElement('tr');
+    row.innerHTML = `
+      <td>${event.title}</td>
+      <td>${formatDate(event.date)}</td>
+      <td>${formatTime(event.time)}</td>
+      <td>${event.venue}</td>
+      <td>${capitalizeFirstLetter(event.category)}</td>
+      <td>
+        <a href="#">View</a>
+        <a href="#">Edit</a>
+        <a href="#" data-index="${index}">Delete</a>
+      </td>
+    `;
 
-  // Create a new event object
-  const eventObj = {
-    title,
-    venue,
-    date,
-    time,
-  };
+    tbody.appendChild(row);
+  });
 
-  // Call a function to add the event to the events list
-  addEventToList(eventObj);
-
-  // Reset the form
-  form.reset();
-});
-
-// Function to add the event to the events list
-function addEventToList(event) {
-  const eventTable = document
-    .getElementById('eventList')
-    .getElementsByTagName('tbody')[0];
-  const row = eventTable.insertRow();
-
-  const titleCell = row.insertCell();
-  titleCell.textContent = event.title;
-
-  const descriptionCell = row.insertCell();
-  descriptionCell.textContent = event.description;
-
-  const dateCell = row.insertCell();
-  dateCell.textContent = event.date;
-
-  const timeCell = row.insertCell();
-  timeCell.textContent = event.time;
-
-  const venueCell = row.insertCell();
-  venueCell.textContent = event.venue;
+  // Attach event listener to delete buttons
+  const deleteButtons = eventList.querySelectorAll('a[data-index]');
+  deleteButtons.forEach((button) => {
+    button.addEventListener('click', handleDelete);
+  });
 }
 
-// Rest of your code
+// Call populateEventList initially to display the events
+populateEventList();
+
+// Create Event Form
+const eventForm = document.getElementById('eventForm');
+
+eventForm.addEventListener('submit', (e) => {
+  e.preventDefault();
+
+  const title = document.getElementById('title').value;
+  const date = document.getElementById('date').value;
+  const time = document.getElementById('time').value;
+  const venue = document.getElementById('venue').value;
+  const category = document.getElementById('category').value;
+
+  // Add the new event to eventsData array
+  eventsData.push({ title, date, time, venue, category });
+
+  // Clear the form inputs
+  eventForm.reset();
+
+  // Update the event list
+  populateEventList();
+
+  // Save eventsData to local storage
+  localStorage.setItem('eventsData', JSON.stringify(eventsData));
+});
+
+// Handle delete event
+function handleDelete(e) {
+  const index = parseInt(e.target.getAttribute('data-index'));
+
+  // Prompt a confirmation message
+  const confirmDelete = confirm('Are you sure you want to delete this event?');
+  if (!confirmDelete) {
+    return; // Do not delete if canceled
+  }
+
+  // Remove the event from eventsData array
+  eventsData.splice(index, 1);
+
+  // Update the event list
+  populateEventList();
+
+  // Save eventsData to local storage
+  localStorage.setItem('eventsData', JSON.stringify(eventsData));
+}
+
+// Helper function to capitalize the first letter
+function capitalizeFirstLetter(str) {
+  return str.charAt(0).toUpperCase() + str.slice(1);
+}
+
+// Helper function to format time in 12-hour format
+function formatTime(time) {
+  const [hours, minutes] = time.split(':');
+  let formattedTime = '';
+
+  // Convert hours to 12-hour format
+  if (hours > 12) {
+    formattedTime += `${hours - 12}:${minutes} PM`;
+  } else if (hours == 12) {
+    formattedTime += `${hours}:${minutes} PM`;
+  } else {
+    formattedTime += `${hours}:${minutes} AM`;
+  }
+
+  return formattedTime;
+}
+
+// Helper function to format date in "mm/dd/yyyy" format
+function formatDate(date) {
+  const [year, month, day] = date.split('-');
+  return `${month}/${day}/${year}`;
+}
